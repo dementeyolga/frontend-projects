a28c0efad1a4b6cf72fed4158a5ed10025de9010
Olga Dementey <dementeyolya@gmail.com>
2023-12-11 21:42:00 +0300

    fix: minor changes

--- coffee-house/scripts/carousel.js
+++ coffee-house/scripts/carousel.js
@@ -5,7 +5,9 @@ const sliderCardImagesList = document.querySelectorAll('.card__image img');
 const sliderArrowLeft = document.querySelector('.slider__arrow.left');
 const sliderArrowRight = document.querySelector('.slider__arrow.right');
 const sliderControlsList = document.querySelectorAll('.slider__control');
-const sliderProgressList = document.querySelectorAll('.slider__control-progress');
+const sliderProgressList = document.querySelectorAll(
+  '.slider__control-progress'
+);
 
 const slideToRight = createSlideToDirection('right');
 const slideToLeft = createSlideToDirection('left');
@@ -17,18 +19,17 @@ let resettingProgressCardNumber;
 
 const autoScrollTime = 5000;
 let sliderAutoScrollIntervalID = setInterval(slideToLeft, autoScrollTime);
-let sliderAutoScrollTimeoutID;
 let scrollIntervalStartTime = Date.now();
 
+let sliderAutoScrollTimeoutID;
 let scrollIntervalContinueTime;
 let scrollIntervalPauseValue = 0;
 
 let launchProgressIntervalID;
+launchProgress(currCardNumber);
 let resetProgressIntervalID;
 
 let pointerStartXCoordinate;
-
-launchProgress(currCardNumber);
 
 sliderCardImagesList.forEach((img) => (img.ondragstart = () => false));
 
@@ -37,144 +38,163 @@ sliderArrowRight.addEventListener('pointerdown', slideToLeft);
 sliderArrowLeft.addEventListener('pointerdown', slideToRight);
 
 sliderCardsWrapper.addEventListener('pointerenter', () => {
-	pauseScroll();
-	pauseProgress();
+  pauseScroll();
+  pauseProgress();
 });
 
 sliderCardsWrapper.addEventListener('pointerleave', () => {
-	resumeScroll();
-	launchProgress(currCardNumber);
+  resumeScroll();
+  launchProgress(currCardNumber);
 });
 
 sliderBody.addEventListener('pointerdown', (ev) => {
-	ev.preventDefault();
-	// sliderBody.setPointerCapture(ev.pointerId);
-	pointerStartXCoordinate = ev.clientX;
-	console.log('pointer down');
+  ev.preventDefault();
+  // sliderBody.setPointerCapture(ev.pointerId);
+  pointerStartXCoordinate = ev.clientX;
+  console.log('pointer down');
 });
 
 sliderBody.addEventListener('pointerup', (ev) => {
-	console.log('pointer UP');
+  console.log('pointer UP');
 
-	let distanceX = ev.clientX - pointerStartXCoordinate;
+  let distanceX = ev.clientX - pointerStartXCoordinate;
 
-	if (Math.abs(distanceX) > 10) {
-		if (distanceX > 0) {
-			slideToRight();
-		} else if (distanceX < 0) {
-			slideToLeft();
-		}
-	}
+  if (Math.abs(distanceX) > 10) {
+    if (distanceX > 0) {
+      slideToRight();
+    } else if (distanceX < 0) {
+      slideToLeft();
+    }
+  }
 });
 
 function createSlideToDirection(direction) {
-	return function () {
-		clearInterval(sliderAutoScrollIntervalID);
-		clearTimeout(sliderAutoScrollTimeoutID);
-		resetIntervalStoredValues();
-		prevCardNumber = currCardNumber;
+  return function () {
+    clearInterval(sliderAutoScrollIntervalID);
+    clearTimeout(sliderAutoScrollTimeoutID);
+    resetIntervalStoredValues();
+    prevCardNumber = currCardNumber;
 
-		if (direction === 'left') {
-			currCardNumber += 1;
-			currCardNumber = currCardNumber > cardsNumber - 1 ? 0 : currCardNumber;
-		} else if (direction === 'right') {
-			currCardNumber -= 1;
-			currCardNumber = currCardNumber < 0 ? cardsNumber - 1 : currCardNumber;
-		}
+    if (direction === 'left') {
+      currCardNumber += 1;
+      currCardNumber = currCardNumber > cardsNumber - 1 ? 0 : currCardNumber;
+    } else if (direction === 'right') {
+      currCardNumber -= 1;
+      currCardNumber = currCardNumber < 0 ? cardsNumber - 1 : currCardNumber;
+    }
 
-		switchControl(prevCardNumber, currCardNumber);
-		sliderAutoScrollIntervalID = setInterval(createSlideToDirection('left'), 5000);
-		scrollIntervalStartTime = Date.now();
+    sliderCardsList[prevCardNumber].addEventListener(
+      'animationend',
+      function () {
+        this.style.display = 'none';
+      },
+      { once: true }
+    );
 
-		sliderCardsList[prevCardNumber].addEventListener(
-			'animationend',
-			function () {
-				this.style.display = 'none';
-			},
-			{ once: true }
-		);
+    sliderCardsList[currCardNumber].style.display = 'flex';
 
-		sliderCardsList[currCardNumber].style.display = 'flex';
+    sliderCardsList[prevCardNumber].classList.remove(
+      'slide-in-right',
+      'slide-in-left',
+      'slide-out-right',
+      'slide-out-left'
+    );
+    sliderCardsList[currCardNumber].classList.remove(
+      'slide-in-right',
+      'slide-in-left',
+      'slide-out-right',
+      'slide-out-left'
+    );
 
-		sliderCardsList[prevCardNumber].classList.remove('slide-in-right', 'slide-in-left', 'slide-out-right', 'slide-out-left');
-		sliderCardsList[currCardNumber].classList.remove('slide-in-right', 'slide-in-left', 'slide-out-right', 'slide-out-left');
+    sliderCardsList[prevCardNumber].classList.add(`slide-out-${direction}`);
+    sliderCardsList[currCardNumber].classList.add(`slide-in-${direction}`);
 
-		sliderCardsList[prevCardNumber].classList.add(`slide-out-${direction}`);
-		sliderCardsList[currCardNumber].classList.add(`slide-in-${direction}`);
-	};
+    switchControl(prevCardNumber, currCardNumber);
+
+    sliderAutoScrollIntervalID = setInterval(
+      createSlideToDirection('left'),
+      autoScrollTime
+    );
+
+    scrollIntervalStartTime = Date.now();
+  };
 }
 
 function pauseScroll() {
-	clearInterval(sliderAutoScrollIntervalID);
-	clearInterval(sliderAutoScrollTimeoutID);
+  clearInterval(sliderAutoScrollIntervalID);
+  clearInterval(sliderAutoScrollTimeoutID);
 
-	const startTime = scrollIntervalContinueTime || scrollIntervalStartTime;
-	scrollIntervalPauseValue += Date.now() - startTime;
-	console.log(scrollIntervalPauseValue);
+  const startTime = scrollIntervalContinueTime || scrollIntervalStartTime;
+  scrollIntervalPauseValue += Date.now() - startTime;
+  console.log(scrollIntervalPauseValue);
 }
 
 function resumeScroll() {
-	scrollIntervalContinueTime = Date.now();
-	sliderAutoScrollTimeoutID = setTimeout(() => {
-		slideToLeft();
-		sliderAutoScrollIntervalID = setInterval(slideToLeft, autoScrollTime);
-	}, autoScrollTime - scrollIntervalPauseValue);
+  scrollIntervalContinueTime = Date.now();
+  sliderAutoScrollTimeoutID = setTimeout(() => {
+    slideToLeft();
+    sliderAutoScrollIntervalID = setInterval(slideToLeft, autoScrollTime);
+  }, autoScrollTime - scrollIntervalPauseValue);
 }
 
 function switchControl(prevIndex, currIndex) {
-	if (currIndex === resettingProgressCardNumber) {
-		clearInterval(resetProgressIntervalID);
-	}
+  if (currIndex === resettingProgressCardNumber) {
+    clearInterval(resetProgressIntervalID);
+  }
 
-	resetProgress(prevIndex);
-	launchProgress(currIndex);
+  console.log(prevIndex, currIndex);
+
+  resetProgress(prevIndex);
+  launchProgress(currIndex);
 }
 
 function resetIntervalStoredValues() {
-	scrollIntervalStartTime = Date.now();
-	scrollIntervalContinueTime = null;
-	scrollIntervalPauseValue = 0;
+  scrollIntervalStartTime = Date.now();
+  scrollIntervalContinueTime = null;
+  scrollIntervalPauseValue = 0;
 }
 
 function resetProgress(prevIndex) {
-	resettingProgressCardNumber = prevIndex;
-	clearInterval(launchProgressIntervalID);
-	const prevProgress = sliderProgressList[prevIndex];
-	let width = prevProgress.style.width;
-	width = +width.slice(0, -1);
+  resettingProgressCardNumber = prevIndex;
+  // clearInterval(launchProgressIntervalID);
+  const prevProgress = sliderProgressList[prevIndex];
+  let width = prevProgress.style.width;
+  width = +width.slice(0, -1);
+  console.log(width);
 
-	resetProgressIntervalID = setInterval(clearProgress, 5);
+  resetProgressIntervalID = setInterval(clearProgress, 5);
 
-	function clearProgress() {
-		if (width < 0) {
-			prevProgress.style.width = '0%';
-			clearInterval(resetProgressIntervalID);
-			resettingProgressCardNumber = null;
-		} else {
-			width -= 1;
-			prevProgress.style.width = `${width}%`;
-		}
-	}
+  function clearProgress() {
+    if (width <= 0) {
+      prevProgress.style.width = '0%';
+      clearInterval(resetProgressIntervalID);
+      resettingProgressCardNumber = null;
+    } else {
+      width -= 1;
+      prevProgress.style.width = `${width}%`;
+      console.log(width);
+    }
+  }
 }
 
 function launchProgress(currIndex) {
-	console.log('launch progress');
-	const currProgress = sliderProgressList[currIndex];
-	let width = currProgress.style.width;
-	width = +width.slice(0, -1);
+  console.log('launch progress');
+  const currProgress = sliderProgressList[currIndex];
+  let width = currProgress.style.width;
+  width = +width.slice(0, -1);
 
-	launchProgressIntervalID = setInterval(updateProgress, autoScrollTime / 1000);
+  launchProgressIntervalID = setInterval(updateProgress, autoScrollTime / 1000);
 
-	function updateProgress() {
-		if (width <= 100) {
-			width += 0.1;
-			currProgress.style.width = `${width}%`;
-		} else {
-			clearInterval(launchProgressIntervalID);
-		}
-	}
+  function updateProgress() {
+    if (width <= 100) {
+      width += 0.1;
+      currProgress.style.width = `${width}%`;
+    } else {
+      clearInterval(launchProgressIntervalID);
+    }
+  }
 }
 
 function pauseProgress() {
-	clearInterval(launchProgressIntervalID);
+  clearInterval(launchProgressIntervalID);
 }
