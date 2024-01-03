bb93093c3ac8ddfab063e5abeffd0991608a3fb0
Olga Dementey <dementeyolya@gmail.com>
2024-01-03 17:23:31 +0300

    feat: start to add basic hangman structure via js

--- hangman/app/hangman.js
+++ hangman/app/hangman.js
@@ -2,10 +2,26 @@ class HangmanGame {
 	constructor() {}
 
 	init() {
+		const game = document.createElement('div');
+		game.className = 'hangman';
+
 		const gameWrapper = document.createElement('div');
-		gameWrapper.classList.add('wrapper');
-		gameWrapper.innerText = 'HANGMAN initiated';
-		document.body.append(gameWrapper);
+		gameWrapper.className = 'wrapper hangman__wrapper';
+		game.append(gameWrapper);
+		document.body.prepend(game);
+
+		const title = document.createElement('h2');
+		title.className = 'section__title hangman__title';
+		title.innerText = 'HANGMAN game';
+		gameWrapper.append(title);
+
+		const gameBody = document.createElement('div');
+		gameBody.className = 'hangman__body';
+		gameWrapper.append(gameBody);
+
+		const gallows = document.createElement('div');
+		gameBody.className = 'hangman__gallows';
+		gameBody.append(gallows);
 	}
 }
 
