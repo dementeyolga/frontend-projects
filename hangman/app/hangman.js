2876d671ad41a4d6ac1eb2f384caa0580ced25d3
Olga Dementey <dementeyolya@gmail.com>
2024-01-05 16:52:02 +0300

    feat: draw hangman man with canvas

--- hangman/app/hangman.js
+++ hangman/app/hangman.js
@@ -29,15 +29,12 @@ class HangmanGame {
 
 		const gameBodyLeft = document.createElement('div');
 		gameBodyLeft.className = 'hangman__body-left';
+		gameBodyLeft.height = '100%';
 		gameBody.append(gameBodyLeft);
 
 		const gameBodyRight = document.createElement('div');
 		gameBodyRight.className = 'hangman__body-right';
 		gameBody.append(gameBodyRight);
-
-		const gallows = document.createElement('div');
-		gallows.className = 'hangman__gallows';
-		gameBodyLeft.append(gallows);
 
 		const input = document.createElement('div');
 		input.className = 'hangman__input';
@@ -69,7 +66,86 @@ class HangmanGame {
 		const keyboard = new VirtualKeyboard();
 		gameBodyRight.append(keyboard.create());
 
-		keyboard.keyboardEl.addEventListener('click', (e) => {
+		const gallows = document.createElement('canvas');
+
+		const canvasWidth = gameBodyLeft.clientWidth;
+		const canvasHeight = gameBodyLeft.clientHeight;
+		console.log(canvasWidth, canvasHeight);
+		gallows.width = canvasWidth;
+		gallows.height = canvasHeight;
+		gallows.className = 'hangman__gallows';
+		gallows.id = 'hangman-gallows';
+		gameBodyLeft.append(gallows);
+
+		const ctx = gallows.getContext('2d');
+		ctx.fillStyle = '#ffffbf';
+		ctx.fillRect(0, 0, gallows.width, gallows.height);
+
+		ctx.beginPath();
+		// ctx.lineCap = "round";
+
+		ctx.lineWidth = 10;
+		ctx.strokeStyle = '#022449';
+
+		//bottom line
+		ctx.moveTo(gallows.width * 0.2, gallows.height - ctx.lineWidth / 2);
+		ctx.lineTo(gallows.width - gallows.width * 0.2, gallows.height - ctx.lineWidth / 2);
+		ctx.stroke();
+
+		// main
+		ctx.moveTo(gallows.width * 0.2 + gallows.width * 0.1, gallows.height);
+		ctx.lineTo(gallows.width * 0.2 + gallows.width * 0.1, gallows.height * 0.2);
+		ctx.stroke();
+
+		//crossbar
+		ctx.moveTo(gallows.width * 0.2 + gallows.width * 0.1, gallows.height * 0.2 + gallows.height * 0.12);
+		ctx.lineTo(gallows.width * 0.2 + gallows.width * 0.1 + gallows.height * 0.12, gallows.height * 0.2);
+		ctx.stroke();
+
+		//to right
+		ctx.moveTo(gallows.width * 0.2 + gallows.width * 0.1 - ctx.lineWidth / 2, gallows.height * 0.2);
+		ctx.lineTo(gallows.width * 0.2 + gallows.width * 0.4, gallows.height * 0.2);
+		ctx.stroke();
+
+		//to bottom
+		ctx.moveTo(gallows.width * 0.2 + gallows.width * 0.4, gallows.height * 0.2 - ctx.lineWidth / 2);
+		ctx.lineTo(gallows.width * 0.2 + gallows.width * 0.4, gallows.height * 0.3);
+		ctx.stroke();
+		ctx.closePath();
+
+		//head
+		ctx.beginPath();
+		ctx.lineWidth = 8;
+		const headRadius = gallows.height * 0.08;
+		ctx.arc(gallows.width * 0.2 + gallows.width * 0.4, gallows.height * 0.3 + headRadius, headRadius, 0, 2 * Math.PI);
+		ctx.stroke();
+
+		//body
+		ctx.moveTo(gallows.width * 0.2 + gallows.width * 0.4, gallows.height * 0.3 + 2 * headRadius);
+		ctx.lineTo(gallows.width * 0.2 + gallows.width * 0.4, gallows.height * 0.3 + 2 * headRadius + gallows.height * 0.25);
+		ctx.stroke();
+
+		//left arm
+		ctx.moveTo(gallows.width * 0.2 + gallows.width * 0.4, gallows.height * 0.3 + 2 * headRadius + gallows.height * 0.04);
+		ctx.lineTo(gallows.width * 0.2 + gallows.width * 0.4 - gallows.width * 0.1, gallows.height * 0.3 + 2 * headRadius + gallows.height * 0.14);
+		ctx.stroke();
+
+		//right arm
+		ctx.moveTo(gallows.width * 0.2 + gallows.width * 0.4, gallows.height * 0.3 + 2 * headRadius + gallows.height * 0.04);
+		ctx.lineTo(gallows.width * 0.2 + gallows.width * 0.4 + gallows.width * 0.1, gallows.height * 0.3 + 2 * headRadius + gallows.height * 0.14);
+		ctx.stroke();
+
+		//left leg
+		ctx.moveTo(gallows.width * 0.2 + gallows.width * 0.4, gallows.height * 0.3 + 2 * headRadius + gallows.height * 0.25);
+		ctx.lineTo(gallows.width * 0.2 + gallows.width * 0.4 - gallows.width * 0.1, gallows.height * 0.3 + 2 * headRadius + gallows.height * 0.25 + gallows.height * 0.1);
+		ctx.stroke();
+
+		//right leg
+		ctx.moveTo(gallows.width * 0.2 + gallows.width * 0.4, gallows.height * 0.3 + 2 * headRadius + gallows.height * 0.25);
+		ctx.lineTo(gallows.width * 0.2 + gallows.width * 0.4 + gallows.width * 0.1, gallows.height * 0.3 + 2 * headRadius + gallows.height * 0.25 + gallows.height * 0.1);
+		ctx.stroke();
+
+		let keyboardHandler = function func(e) {
 			if (e.target.classList.contains('correct') || e.target.classList.contains('disabled')) return;
 			console.log('ck');
 			const chosenLetter = e.target.textContent.toLowerCase();
@@ -84,16 +160,24 @@ class HangmanGame {
 					while ((pos = currentWord.indexOf(chosenLetter, pos + 1)) != -1) {
 						input.children[pos].innerText = chosenLetter.toUpperCase();
 					}
+
+					if ([...input.children].every((el) => el.textContent.trim() !== '_')) {
+						console.log('you won the game');
+						e.currentTarget.removeEventListener('click', func);
+					}
 				} else {
 					e.target.classList.add('disabled');
 					incorrectGuesses.textContent = +incorrectGuesses.textContent + 1;
 
 					if (+incorrectGuesses.textContent >= 6) {
 						console.log('oops, game over');
+						e.currentTarget.removeEventListener('click', func);
 					}
 				}
 			}
-		});
+		};
+
+		keyboard.keyboardEl.addEventListener('click', keyboardHandler);
 	}
 }
 
