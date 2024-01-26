cd5a4d4d9d35a7a1ba2b56c688d239c73b81b0a6
Olga Dementey <dementeyolya@gmail.com>
2024-01-26 16:47:29 +0300

    feat: update webpack config for development

--- nonograms/src/index.js
+++ nonograms/src/index.js
@@ -1,10 +1,19 @@
+import test from './test';
 import './style.css';
+import Lavender from './lavender.jpg';
 
 function component() {
 	const element = document.createElement('div');
 
 	element.innerHTML = 'Hello';
 	element.classList.add('hello');
+
+	const lavender = new Image();
+	lavender.src = Lavender;
+
+	element.appendChild(lavender);
+
+	test();
 
 	return element;
 }
