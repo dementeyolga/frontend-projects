9c1910ea344c4d4fa2e94d070647645282f1d338
Olga Dementey <dementeyolya@gmail.com>
2024-01-28 19:27:55 +0300

    feat: update game-header component to take content from a template

--- nonograms/src/components/gameHeader/gameHeader.js
+++ nonograms/src/components/gameHeader/gameHeader.js
@@ -1,9 +1,18 @@
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    :host {
+      display: flex;
+      background: red;
+    }
+  </style>
+
+  <div className="wrapper">Nonograms</div> 
+`;
 class GameHeader extends HTMLElement {
   connectedCallback() {
     this.attachShadow({ mode: 'open' });
-    this.shadowRoot.innerHTML = `
-      Nonograms
-    `;
+    this.shadowRoot.append(template.content.cloneNode(true));
   }
 }
 
