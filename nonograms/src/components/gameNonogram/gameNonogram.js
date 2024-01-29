49a78450841e78bca9bb876fe7a8342598fa7071
Olga Dementey <dementeyolya@gmail.com>
2024-01-29 22:12:11 +0300

    feat: start to implement nonogram hints

--- nonograms/src/components/gameNonogram/gameNonogram.js
+++ nonograms/src/components/gameNonogram/gameNonogram.js
@@ -7,12 +7,16 @@ customElements.define('game-field', GameField);
 const nonogramStyles = document.createElement('style');
 nonogramStyles.textContent = nonogramStylesStr;
 
+const matrix = nonograms[0].matrix;
+const correctSolution = matrix.flat().join('').toString();
+console.log(correctSolution);
+
 const template = document.createElement('template');
 template.innerHTML = `
 	<div class="picture">Picture</div>
-	<div class="top-pane">top-pane</div>
-	<div class="left-pane">left-pane</div>
-  <game-field level="${nonograms[0].matrix.length}"></game-field>
+	<div class="top-pane"></div>
+	<div class="left-pane"></div>
+  <game-field level="${matrix.length}"></game-field>
 `;
 
 class GameNonogram extends HTMLElement {
@@ -21,8 +25,59 @@ class GameNonogram extends HTMLElement {
     shadowRoot.append(template.content.cloneNode(true));
     shadowRoot.append(nonogramStyles);
 
-    const topPane = shadowRoot.getElementById('top-pane');
-    const leftPane = shadowRoot.getElementById('left-pane');
+    const topPane = shadowRoot.querySelector('.top-pane');
+    const leftPane = shadowRoot.querySelector('.left-pane');
+
+    for (let i = 0; i < matrix.length; i += 1) {
+      const leftHint = document.createElement('div');
+      leftHint.classList.add('left-pane__hint');
+
+      const topHint = document.createElement('div');
+      topHint.classList.add('top-pane__hint');
+
+      let counterLeft = 0;
+      let counterTop = 0;
+      for (let j = 0; j < matrix.length; j += 1) {
+        if (matrix[i][j]) {
+          counterLeft += 1;
+        }
+
+        if (
+          (counterLeft && !matrix[i][j]) ||
+          (counterLeft && j === matrix.length - 1)
+        ) {
+          leftHint.insertAdjacentHTML(
+            'beforeend',
+            `
+							<div class="left-pane__number">${counterLeft}</div>
+						`
+          );
+
+          counterLeft = 0;
+        }
+
+        if (matrix[j][i]) {
+          counterTop += 1;
+        }
+
+        if (
+          (counterTop && !matrix[j][i]) ||
+          (counterTop && j === matrix.length - 1)
+        ) {
+          topHint.insertAdjacentHTML(
+            'beforeend',
+            `
+						<div class="top-pane__number">${counterTop}</div>
+						`
+          );
+
+          counterTop = 0;
+        }
+      }
+
+      leftPane.append(leftHint);
+      topPane.append(topHint);
+    }
   }
 }
 
