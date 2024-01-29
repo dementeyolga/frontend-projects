d3b51e49270681d9225b8082df58fc8e7c98192b
Olga Dementey <dementeyolya@gmail.com>
2024-01-29 21:24:55 +0300

    feat: start to implement nonogram hints

--- nonograms/src/components/gameNonogram/gameNonogram.js
+++ nonograms/src/components/gameNonogram/gameNonogram.js
@@ -9,15 +9,21 @@ nonogramStyles.textContent = nonogramStylesStr;
 
 const template = document.createElement('template');
 template.innerHTML = `
+	<div class="picture">Picture</div>
+	<div class="top-pane">top-pane</div>
+	<div class="left-pane">left-pane</div>
   <game-field level="${nonograms[0].matrix.length}"></game-field>
 `;
 
 class GameNonogram extends HTMLElement {
-	connectedCallback() {
-		const shadowRoot = this.attachShadow({ mode: 'open' });
-		shadowRoot.append(template.content.cloneNode(true));
-		shadowRoot.append(nonogramStyles);
-	}
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.append(template.content.cloneNode(true));
+    shadowRoot.append(nonogramStyles);
+
+    const topPane = shadowRoot.getElementById('top-pane');
+    const leftPane = shadowRoot.getElementById('left-pane');
+  }
 }
 
 export { GameNonogram };
