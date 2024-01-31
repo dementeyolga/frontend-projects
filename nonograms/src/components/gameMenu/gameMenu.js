57c5b1612f16a1e1bb1235c745a84610f36fde9a
Olga Dementey <dementeyolya@gmail.com>
2024-01-31 17:26:55 +0300

    fix: fix typo - 'className' insted of 'class' in HTML elements

--- nonograms/src/components/gameMenu/gameMenu.js
+++ nonograms/src/components/gameMenu/gameMenu.js
@@ -7,32 +7,32 @@ menuStyles.textContent = menuStyleStr;
 const levels = [...new Set(nonograms.map((item) => item.level))];
 
 const levelsHTML = levels
-  .map((level) => {
-    const gameNames = nonograms
-      .filter((item) => item.level === level)
-      .map((item) => `<div class="menu__item">${item.name}</div>\n`)
-      .join('\n');
+	.map((level) => {
+		const gameNames = nonograms
+			.filter((item) => item.level === level)
+			.map((item) => `<div class="menu__item">${item.name}</div>\n`)
+			.join('\n');
 
-    return `
+		return `
     <div class="level">
-      <h3 className="level__title">${level}</h3>
-      <div className="level__games">
+      <h3 class="level__title">${level}</h3>
+      <div class="level__games">
         ${gameNames}
       </div>
     </div>
   `;
-  })
-  .join('\n');
+	})
+	.join('\n');
 
 const template = document.createElement('template');
 template.innerHTML = levelsHTML;
 
 class GameMenu extends HTMLElement {
-  connectedCallback() {
-    const shadowRoot = this.attachShadow({ mode: 'open' });
-    shadowRoot.append(template.content.cloneNode(true));
-    shadowRoot.append(menuStyles);
-  }
+	connectedCallback() {
+		const shadowRoot = this.attachShadow({ mode: 'open' });
+		shadowRoot.append(template.content.cloneNode(true));
+		shadowRoot.append(menuStyles);
+	}
 }
 
 export { GameMenu };
