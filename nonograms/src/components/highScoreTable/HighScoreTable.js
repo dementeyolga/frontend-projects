011997a0967d76800a891e1c79b4844ff209fdd1
Olga Dementey <dementeyolya@gmail.com>
2024-02-07 15:40:44 +0300

    fix: minor enhancements

--- nonograms/src/components/highScoreTable/HighScoreTable.js
+++ nonograms/src/components/highScoreTable/HighScoreTable.js
@@ -5,32 +5,39 @@ class HighScoreTable extends HTMLElement {
     const shadowRoot = this.attachShadow({ mode: 'open' });
 
     const template = document.createElement('template');
-    let results = JSON.parse(localStorage.getItem('highScoreTable')).slice(
-      0,
-      5
-    );
-    results.sort((a, b) => a.duration - b.duration);
-    results = results.map(
-      (result, i) => `  <tr class="high-scores__score">
-                      <td>${i + 1}.</td>
-                      <td>${result.time}</td>
-                      <td>${result.level}</td>
-                      <td>${result.name}</td>
-                    </tr>
-                  `
-    );
+    let results = JSON.parse(localStorage.getItem('highScoreTable'));
+
+    if (results) {
+      results = results.slice(0, 5);
+      results.sort((a, b) => a.duration - b.duration);
+      results = results.map(
+        (result, i) => `  <tr class="high-scores__score">
+                        <td>${i + 1}.</td>
+                        <td>${result.time}</td>
+                        <td>${result.level}</td>
+                        <td>${result.name}</td>
+                      </tr>
+                    `
+      );
+    }
 
     template.innerHTML = `
     <div class="high-scores">
       <h2>Latest 5 scores:</h2>
       <table class="high-scores__scores">
+        ${
+          results
+            ? `
         <tr class="high-scores__score header">
-          <th>No</th>
-          <th>Time</th>
-          <th>Level</th>
-          <th>Game name</th>
+        <th>No</th>
+        <th>Time</th>
+        <th>Level</th>
+        <th>Game name</th>
         </tr>
-        ${results.join('\n')}
+        `
+            : ''
+        } 
+        ${results ? results.join('\n') : '<tr><td colspan="4">No scores yet.</td></tr>'}
       </table>
     </div>
 `;
