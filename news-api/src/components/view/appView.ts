d3505a511011a83a8d105a8ca5cca4b3053532b5
Olga Dementey <dementeyolya@gmail.com>
2024-02-23 22:04:38 +0300

    feat: migrate appView.js to typescript

---
+++ news-api/src/components/view/appView.ts
@@ -0,0 +1,36 @@
+import News from './news/news';
+import Sources from './sources/sources';
+import { NewsItem } from '../../types/interfaces';
+import { NewsResponse } from '../../types/interfaces';
+import { SourceItem } from '../../types/interfaces';
+import { SourcesResponse } from '../../types/interfaces';
+
+export class AppView {
+    protected news: News;
+    protected sources: Sources;
+
+    constructor() {
+        this.news = new News();
+        this.sources = new Sources();
+    }
+
+    drawNews(data: NewsResponse): void {
+        if (!data) {
+            throw new Error('Data not provided to drawNews function');
+        } else {
+            const values: NewsItem[] = data.articles ? data.articles : [];
+            this.news.draw(values);
+        }
+    }
+
+    drawSources(data: SourcesResponse): void {
+        if (!data) {
+            throw new Error('Data not provided to drawSources function');
+        } else {
+            const values: SourceItem[] = data.sources ? data.sources : [];
+            this.sources.draw(values);
+        }
+    }
+}
+
+export default AppView;
