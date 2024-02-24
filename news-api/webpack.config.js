9d4ddad795350f10d49bd2078a25432d135b1b7b
Olga Dementey <dementeyolya@gmail.com>
2024-02-24 18:56:03 +0300

    feat: add favicon

--- news-api/webpack.config.js
+++ news-api/webpack.config.js
@@ -32,6 +32,7 @@ const baseConfig = {
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
+            favicon: path.resolve(__dirname, './src/assets/img/favicon.ico'),
         }),
         new CleanWebpackPlugin(),
     ],
