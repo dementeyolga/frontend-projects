495f2021df16ee5b5dac186bba1bcab4fca5e384
dementeyolga <60576661+dementeyolga@users.noreply.github.com>
2024-03-07 16:19:10 +0300

    core: RSS-PZ-34 Setup Configs
    * core: RSS-PZ-34 Setup Configs (prettier config)
    
    * core: RSS-PZ-34 Setup Configs (setup eslint to work with TS and prettier)
    
    * core: RSS-PZ-34 Setup Configs (try to degrafe eslint config to a stable one)
    
    * core: RSS-PZ-34 Setup Configs (resetup prettier, eslint with airbnb, typescript to run together)
    
    * core: RSS-PZ-34 Setup Configs (setup Husky)
    
    * core: RSS-PZ-34 Setup Configs (noInlineConfig: true)
    
    * core: RSS-PZ-34 Setup Config (basic Vite setup)

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  ignorePatterns: ["**/*.js"],
+  extends: [
+    "eslint:recommended",
+    "airbnb-base",
+    "plugin:@typescript-eslint/recommended",
+    "airbnb-typescript/base",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "@typescript-eslint/no-explicit-any": "error",
+  },
+  noInlineConfig: true,
+};
